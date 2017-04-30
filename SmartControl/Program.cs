517b3193733ad72e0a694cfeaf4ae64c74c2f507
Zat <zat@zat.ddns.net>
2017-04-30 21:10:09 +0200

    Projektdateien hinzufügen.

---
+++ SmartControl/Program.cs
@@ -0,0 +1,73 @@
+﻿using log4net;
+using log4net.Config;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SmartControl
+{
+    class Program
+    {
+        private static string CONFIG_FILE = "SmartControlConfigs.json";
+        private static ServerConfiguration[] serverConfigs;
+        public static ILog Logger { get; private set; } =  LogManager.GetLogger("SMRTCNTRL");
+
+        private static void LoadConfigs()
+        {
+            Logger.Info("Loading server configurations...");
+            if (!File.Exists(CONFIG_FILE))
+                throw new FileNotFoundException("Couldn't load configs", CONFIG_FILE);
+
+            using (var reader = new StreamReader(CONFIG_FILE))
+            {
+                Logger.Info("Parsing server configurations...");
+                serverConfigs = (ServerConfiguration[])new JsonSerializer().Deserialize(reader, typeof(ServerConfiguration[]));
+            }
+        }
+
+        private static void CreateConfigs()
+        {
+            Logger.Info("Creating sample configuration files...");
+
+            var cfg = new ServerConfiguration();
+            cfg.Name = "SampleConfiguration";
+            cfg.FilePath = "/some/path";
+            cfg.WorkingDirectory = "/some/directory";
+            cfg.Arguments = "-a -r -g -s";
+            
+            var cfgs = new ServerConfiguration[] { cfg};
+            using (var writer = new StreamWriter(CONFIG_FILE))
+                new JsonSerializer() { Formatting = Formatting.Indented }.Serialize(writer, cfgs);
+        }
+
+        static void Main(string[] args)
+        {
+            DOMConfigurator.Configure();
+            try
+            {
+                Logger.Info("Logger initialized!");
+
+                LoadConfigs();
+
+                Logger.Info(string.Format("Loaded {0} config(s):", serverConfigs.Length));
+
+                foreach (var config in serverConfigs)
+                    Logger.Info(string.Format("\tLoaded configuration \"{0}\"", config.Name));
+            }
+            catch (FileNotFoundException ex)
+            {
+                Logger.Error("You're missing the configuration-file", ex);
+                CreateConfigs();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("SmartControl crashed", ex);
+            }
+            Logger.Info("Exiting...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SmartControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("SmartControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmartControl")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("916b85eb-97ce-4c92-97c9-6e8074046f27")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
